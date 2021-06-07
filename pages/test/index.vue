312c4026e05b9911d932c92724ab7e1ff58366ee
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2021-06-08 01:11:30 +0700

    add logout

---
+++ pages/test/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ag-grid-vue></ag-grid-vue>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+  auth: false,
+
+  data() {
+    return {};
+  },
+};
+</script>
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './{components,layouts,pages}/**/*.vue'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
