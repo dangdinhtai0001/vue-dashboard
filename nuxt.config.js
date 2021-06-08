59d2e5779b44cda8971292cc79cc6fd3edeab451
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2021-06-08 09:11:15 +0700

    remove ag grid

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,8 +21,6 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'ag-grid-community/dist/styles/ag-grid.css',
-    'ag-grid-community/dist/styles/ag-theme-balham.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
